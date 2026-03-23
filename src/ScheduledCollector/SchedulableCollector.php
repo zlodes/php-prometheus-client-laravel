f7d3c8ea787f330650aa02c73e04af429e7777cb
Zlatoslav Desiatnikov <px.zlodes@gmail.com>
2026-03-23 19:48:32 +0300

    chore: rollback psalm annotations

--- src/ScheduledCollector/SchedulableCollector.php
+++ src/ScheduledCollector/SchedulableCollector.php
@@ -6,9 +6,6 @@ namespace Zlodes\PrometheusClient\Laravel\ScheduledCollector;
 
 use Zlodes\PrometheusClient\Laravel\Exception\CannotCollectScheduledMetrics;
 
-/**
- * @psalm-immutable
- */
 interface SchedulableCollector
 {
     /**
--- src/ScheduledCollector/SchedulableCollectorRegistry.php
+++ src/ScheduledCollector/SchedulableCollectorRegistry.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Zlodes\PrometheusClient\Laravel\ScheduledCollector;
 
-/**
- * @psalm-mutable
- */
 interface SchedulableCollectorRegistry
 {
     /**
